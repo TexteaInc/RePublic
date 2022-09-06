42a6170b205256e85a4328386bfe67b8457486bb
himself65 <himself65@outlook.com>
2022-09-05 19:22:38 -0500

    fix: cannot run code

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from pydatafront.decorator import textea_export
 
-def triple(x:int): # this function should NOT be hosted
-  return x*3
 
-@textea_export()  # use all default settings
-def triple_plus(x:int):  # this function should be hosted at redstone.textea.io/USER_NAME/PROJECT_NAME/FUNCTION_NAME
-  return triple(x)+1 
+def triple(x: int):  # this function should NOT be hosted
+    return x * 3
 
 
+@textea_export("/triple_plus",
+               x={
+                   "treat_as": "column"
+               }, )  # use all default settings
+def triple_plus(x: int):  # this function should be hosted at redstone.textea.io/USER_NAME/PROJECT_NAME/FUNCTION_NAME
+    return triple(x) + 1
