30aa30ea0c6e9501eb5b44f49a340bf2c2c404c3
Himself65 <himself65@outlook.com>
2022-11-20 15:28:45 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from pydatafront.decorator import textea_export
-
+from funix.decorator import funix_export
 
 def triple(x: int):  # this function should NOT be hosted
     return x * 3
 
 
-@textea_export("/triple_plus",
+@funix_export("/triple_plus",
                x={
                    "treat_as": "column"
                }, )  # use all default settings
@@ -13,7 +12,7 @@ def triple_plus(x: int):  # this function should be hosted at redstone.textea.io
     return triple(x) + 1
 
 
-@textea_export()
+@funix_export()
 def file_read() -> str:
     """Simply read a file in this repo and return the first line as a string
     """
